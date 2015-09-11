3339c83619286e026dcb87b776aa95ea3bd8418c
Turtle-Head <djinn.jecture@gmail.com>
2015-09-11 14:53:14 -0500

    Attempting revert

--- js/app.js
+++ js/app.js
@@ -24,10 +24,9 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 var Player = function() {
-    this.sprite = 'images/char-horn-girl.png';
+    this.sprite = 'images/char-boy.png';
 }
 
 Player.prototype.update = function(dt) {
@@ -41,12 +40,7 @@ Player.prototype.render = function() {
 Player.prototype.handleInput = function(keyPress) {
 
 }
-=======
 
->>>>>>> parent of 01ab91c... Added prototypes and variables for initial groundwork
-=======
-
->>>>>>> parent of 01ab91c... Added prototypes and variables for initial groundwork
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
